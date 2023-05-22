5462c1ce0812fe832e4ae356d40eb1f18d428793
박근홍 <106947027+Omakmusae@users.noreply.github.com>
2023-05-22 21:51:00 +0900

    Revert "feat : 회원가입 엔티티 생성"

--- src/main/java/com/example/finalproject12be/domain/member/entity/Member.java
+++ src/main/java/com/example/finalproject12be/domain/member/entity/Member.java
@@ -1,33 +1,7 @@
 package com.example.finalproject12be.domain.member.entity;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
 @Entity
 public class Member {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "MEMBER_ID")
-	private Long id;
-
-	@Column(nullable = false)
-	private String email;
-
-	@Column(nullable = false)
-	private String password;
-
-	@Column(nullable = false)
-	private String nickname;
-
-
-
 }
