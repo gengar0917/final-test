16c17b63acf1fe1d664a87065ae82e01bc93c64e
kes <kmskes1125@gmail.com>
2023-05-22 17:36:13 +0900

    initial commit

---
+++ src/main/java/com/example/finalproject12be/domain/member/repository/MemberRepository.java
@@ -0,0 +1,6 @@
+package com.example.finalproject12be.domain.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository {
+}
