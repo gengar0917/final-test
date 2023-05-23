28d854af7268416918d9934066e233ae355ffb17
ãyoobeommo <kiekangmin@naver.com>
2023-05-23 16:20:19 +0900

    Fix : jasyPT 오류 수정
    encrypt 오류 수정 (url 오기입)

--- src/main/java/com/example/finalproject12be/Finalproject12BeApplication.java
+++ src/main/java/com/example/finalproject12be/Finalproject12BeApplication.java
@@ -1,9 +1,13 @@
 package com.example.finalproject12be;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
+@EnableEncryptableProperties
 public class Finalproject12BeApplication {
 
     public static void main(String[] args) {
