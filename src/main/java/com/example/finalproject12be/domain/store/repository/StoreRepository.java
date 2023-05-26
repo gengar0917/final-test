e4f460a07811cde00788907df564e0e47a9cf0ff
kes <kmskes1125@gmail.com>
2023-05-26 13:55:27 +0900

    Feat: DB에 서울 약국 저장 api

---
+++ src/main/java/com/example/finalproject12be/domain/store/repository/StoreRepository.java
@@ -0,0 +1,9 @@
+package com.example.finalproject12be.domain.store.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.finalproject12be.domain.store.entity.Store;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
+
+}
