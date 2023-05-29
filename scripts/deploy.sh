cfb6a254fa75d9bc10feeffe0186e347272211d0
ãyoobeommo <kiekangmin@naver.com>
2023-05-29 18:47:33 +0900

    Fix : cicd 관련 jar 파일 생성 오류 수정 4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,13 +7,13 @@ APP_NAME=finalproject-12BE
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+CURRENT_PID=$(pgrep -fl $APP_NAME | grep jar | awk '{print $1}')
 
 if [ -z $CURRENT_PID ]
 then
   echo "> 종료할 애플리케이션이 없습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
+  echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
